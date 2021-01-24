8b795f7920bca5f655ed3ea4b488d226af2d3bc5
RudaF <floresta.ruda@gmail.com>
2021-01-24 19:01:31 -0300

    Added some styles to main page

--- js/kingdomClass.js
+++ js/kingdomClass.js
@@ -1,3 +1,13 @@
 class Kingdom {
-  constructor() {}
+  constructor() {
+    this.church = 50;
+    this.population = 50;
+    this.army = 50;
+    this.economy = 50;
+  }
+
+  updateKingdom() {
+    // Based on a decision the player makes
+    // update the kindom status
+  }
 }
