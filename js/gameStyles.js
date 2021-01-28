121351ff2879dae60e4c1d795606178b39f15dd8
RudaF <floresta.ruda@gmail.com>
2021-01-28 15:37:33 -0300

    Removed some non-using files

--- js/gameStyles.js
+++
@@ -1,7 +0,0 @@
-// document.getElementById("single-choice-button").onclick = () => {
-//   document
-//     .getElementById("right-choice-button")
-//     .classList.add("animateToRight");
-//   document.getElementById("left-choice-button").classList.add("animateToLeft");
-//   document.getElementById("single-choice-button").classList.add("hide");
-// };
